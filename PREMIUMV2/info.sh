07dbd555ee10284d9a3d43c8c453f83f8f5d0559
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-05-24 05:30:42 +0700

    added information service

---
+++ PREMIUMV2/info.sh
@@ -0,0 +1,25 @@
+Info Port:
+   ----- XRAY CORE ----------
+   - XRAY VLESS XTLS SPLICE  : 443
+   - XRAY VLESS XTLS DIRECT  : 443
+   - XRAY VLESS WS TLS       : 443
+   - XRAY TROJAN TCP         : 443
+   - XRAY VMESS TLS          : 443
+   - XRAY VLESS GRPC         : 6643
+   - XRAY VLESS H2           : 100
+   - XRAY VLESS MKCP         : 7443
+   - XRAY VLESS MKCP TLS     : 743
+   - TROJAN GO               : 2087
+   - XRAY TROJAN GRPC TLS    : 653
+   - XRAY TROJAN XTLS/TLS    : 6443
+   - XRAY VMESS HTTP TLS     : 643
+   - XRAY VMESS HTTP         : 80
+   - XRAY VMESS TCP TLS      : 535
+   - XRAY VLESS WS NONE      : 88
+   - XRAY VMESS WS NONE      : 888
+   - XRAY Shadowsocks AEAD   : 1111
+   - XRAY TROJAN GRPC        : 2083
+   - XRAY TROJAN HTTP        : 880
+   - XRAY TROJAN XTLS        : 5443
+   - XRAY TROJAN WS TLS      : 2096
+   - XRAY TROJAN WS NON TLS  : 2095
