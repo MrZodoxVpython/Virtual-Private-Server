a823d9fdc4506e3beceb9e13b173039571a6a81b
MrZodoxVpython <86802164+MrZodoxVpython@users.noreply.github.com>
2025-05-26 01:36:14 +0700

    Update add-tr.sh
    fix bugs loyout script

--- VPS/requirements/req-setup/xray/trojan/add-tr.sh
+++ VPS/requirements/req-setup/xray/trojan/add-tr.sh
@@ -67,7 +67,6 @@ echo -e "Link none TLS  : ${trojanlink2}" | tee -a /etc/log-create-trojan.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-trojan.log
 echo -e "Link gRPC      : ${trojanlink1}" | tee -a /etc/log-create-trojan.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-trojan.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-trojan.log
 echo "" | tee -a /etc/log-create-trojan.log
 read -n 1 -s -r -p "Press any key to back on menu"
 #echo -e "User : $user\nUUID : $uuid\nDomain : $domain\nTLS : $tls\nExpired : $exp\n------" >> /etc/log-create-trojan.log
