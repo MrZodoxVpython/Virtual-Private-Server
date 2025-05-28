2e7ce8e0824b5cb3054240f8dc46478086f6595f
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-05-28 08:19:45 +0700

    adding improvement code, bug resolv and change the credential

--- VPS/requirements/req-setup/client/restore.sh
+++ VPS/requirements/req-setup/client/restore.sh
@@ -11,12 +11,13 @@ tar -xzf /root/backup-vpn.tar.gz -C /root/
 cp -r /root/backup-vpn/xray /etc/
 cp -r /root/backup-vpn/v2ray /etc/ 2>/dev/null
 cp -f /root/backup-vpn/passwd /etc/
-cp -f /root/backup-vpn/shadow /etc/
-cp -f /root/backup-vpn/group /etc/
-cp -f /root/backup-vpn/gshadow /etc/
 cp -r /root/backup-vpn/cron.d /etc/
 cp -r /root/backup-vpn/ssh /etc/
 cp -r /root/backup-vpn/system /etc/systemd/
+
+cp -f /root/backup-vpn/shadow /etc/
+cp -f /root/backup-vpn/group /etc/
+cp -f /root/backup-vpn/gshadow /etc/
 
 # Restart layanan
 systemctl daemon-reexec
