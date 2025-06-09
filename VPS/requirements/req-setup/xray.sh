9086a732ad6164597644eabd16af5eb5f3f3aa11
MrZodoxVpython <86802164+MrZodoxVpython@users.noreply.github.com>
2025-06-09 19:18:41 +0700

    fix bug

--- VPS/requirements/req-setup/xray.sh
+++ VPS/requirements/req-setup/xray.sh
@@ -55,6 +55,7 @@ EOF
 
 # Reload systemd & aktifkan service
 echo -e "${Yellow}[•] Enabling Xray service...${NC}"
+touch /root/log-install.txt
 mkdir -p /var/log/xray
 touch /var/log/xray/access.log /var/log/xray/error.log
 chown -R nobody:nogroup /var/log/xray
