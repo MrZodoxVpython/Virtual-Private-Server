55a397716f041a5c583d32cf8c9954d6a80de387
MrZodoxVpython <86802164+MrZodoxVpython@users.noreply.github.com>
2025-06-30 01:02:32 +0700

    Update webadmin.sh

--- VPS/requirements/req-setup/webadmin.sh
+++ VPS/requirements/req-setup/webadmin.sh
@@ -49,32 +49,57 @@ function menu_webadmin() {
             PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;")
             PHP_FPM_SERVICE="php${PHP_VERSION}-fpm"
 
-            echo -e "${YELLOW}🌐 Masukkan domain untuk akses panel (Contoh: panel.domainkamu.com):${NC}"
+            echo -e "${YELLOW}🌐 Masukkan domain website kamu (Contoh: panel.tokomard.store):${NC}"
             read -p "➤ " DOMAIN_PANEL
 
-            mkdir -p /var/www/panel
-            curl -s https://raw.githubusercontent.com/namaskuy/webadmin-xray/main/index.php -o /var/www/panel/index.php
-            chown -R www-data:www-data /var/www/panel
+            mkdir -p /var/www/html
+            curl -s https://raw.githubusercontent.com/MrZodoxVpython/Website-Tokomard-Panel -o /var/www/html
+            chown -R www-data:www-data /var/www/html
 
             cat > /etc/caddy/Caddyfile <<EOF
-            ${DOMAIN_PANEL} {
-            # Reverse proxy untuk Xray (WS TLS)
-                sgdo-2dev.tokomard.store {
-                # Sesuaikan path dan port sesuai konfigurasi Xray Anda
-                reverse_proxy /vmess 127.0.0.1:23456
-                reverse_proxy /vless-ws 127.0.0.1:14016
-                reverse_proxy /trojan-ws 127.0.0.1:25432
-                reverse_proxy /ss-ws 127.0.0.1:30300
-                encode gzip
-            }
-            # website statis HTML
-                panel.tokomard.store {
-                root * /var/www/html/xray-panel
-                try_files {path} {path}/ /index.html /index.php
-                php_fastcgi unix//run/php/php7.4-fpm.sock
-                file_server
-            }
-            EOF
+# Reverse proxy untuk Xray (WS TLS)
+sgdo-2dev.tokomard.store {
+    reverse_proxy /trojan-ws 127.0.0.1:25432 {
+        transport http {
+            versions h1
+            read_buffer 0
+        }
+    }
+    reverse_proxy /vmess 127.0.0.1:23456 {
+        transport http {
+            versions h1
+            read_buffer 0
+        }
+    }
+    reverse_proxy /vless 127.0.0.1:14016 {
+        transport http {
+            versions h1
+            read_buffer 0
+        }
+    }
+    reverse_proxy /ss-ws 127.0.0.1:30300 {
+        transport http {
+            versions h1
+            read_buffer 0
+        }
+    }
+}
+
+
+# website statis HTML
+${DOMAIN_PANEL} {
+    root * /var/www/html/Website-Tokomard-Panel
+    try_files {path} {path}/ /index.html /index.php
+    php_fastcgi unix//run/php/${PHP_VERSION}-fpm.sock
+    file_server
+}
+klinik-gua.tokomard.store {
+    root * /var/www/html/Project-Akhir-UAS
+    try_files {path} {path}/ /index.html /index.php
+    php_fastcgi unix//run/php/php7.4-fpm.sock
+    file_server
+}
+EOF
 
             systemctl restart $PHP_FPM_SERVICE
             systemctl enable $PHP_FPM_SERVICE
@@ -90,9 +115,9 @@ function menu_webadmin() {
         2)
             banner
             mkdir -p /var/www/html/panel
-            curl -s https://raw.githubusercontent.com/namaskuy/webadmin-xray/main/index.php -o /var/www/html/panel/index.php
+            curl -s https://raw.githubusercontent.com/MrZodoxVpython/Website-Tokomard-Panel/main/index.php -o /var/www/html/index.php
             echo -e "${GREEN}🔧 Menjalankan PHP Web Server di port 8080...${NC}"
-            nohup php -S 0.0.0.0:8080 -t /var/www/html/panel >/dev/null 2>&1 &
+            nohup php -S 0.0.0.0:8080 -t /var/www/html/ >/dev/null 2>&1 &
             echo -e "${GREEN}✅ Web panel aktif di: http://${IPVPS}:8080${NC}"
             read -n 1 -s -r -p "Tekan tombol apapun untuk kembali..."
             menu_webadmin
@@ -109,7 +134,7 @@ function menu_webadmin() {
 
         4)
             banner
-            EDITOR_PATH="/var/www/panel/index.php"
+            EDITOR_PATH="/var/www/html/index.php"
             if [ -f "$EDITOR_PATH" ]; then
                 nano "$EDITOR_PATH"
             else
@@ -123,17 +148,19 @@ function menu_webadmin() {
             banner
             echo -e "${YELLOW}📁 Folder yang tersedia di /var/www/html:${NC}"
             ls -1 /var/www/html
-            echo -e "\n${YELLOW}🔧 Masukkan nama folder yang ingin dijadikan root Caddy (contoh: facebook):${NC}"
+            echo -e "\n${YELLOW}🔧 Masukkan nama folder yang ingin dijadikan root Caddy (contoh: Website-Tokomard-Panel):${NC}"
             read -p "➤ " FOLDER
 
             if [ -d "/var/www/html/$FOLDER" ]; then
-                echo -e "${YELLOW}🌐 Masukkan domain untuk folder ini (contoh: facebook.domainkamu.com):${NC}"
+                echo -e "${YELLOW}🌐 Masukkan domain untuk folder ini (contoh: panel.tokomard.store):${NC}"
                 read -p "➤ " DOMAIN_CUSTOM
 
                 echo "${DOMAIN_CUSTOM} {
-            root * /var/www/html/${FOLDER}
-            file_server
-            }" >> /etc/caddy/Caddyfile
+                      root * /var/www/html/${FOLDER}
+                      try_files {path} {path}/ /index.html /index.php
+                      php_fastcgi unix//run/php/php7.4-fpm.sock
+                      file_server
+                 }" >> /etc/caddy/Caddyfile
 
                 systemctl reload caddy
                 echo -e "${GREEN}✅ Domain ${DOMAIN_CUSTOM} sekarang mengarah ke folder /var/www/html/${FOLDER}${NC}"
@@ -155,7 +182,7 @@ function menu_webadmin() {
             sleep 1
             menu_webadmin
             ;;
-            esac
+    esac
 }
 
 menu_webadmin
